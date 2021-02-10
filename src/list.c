200f842175a942ae7901f2b24a87a2a563d5e049
Luis Santos <santosluisjay@gmail.com>
2021-02-10 01:09:36 +0000

    HW1 Done

--- src/list.c
+++ src/list.c
@@ -12,6 +12,7 @@ void list_add(struct list_element *head, struct list_element* ele){
 	temp->prev = prev;
 	temp->next = ele;
 }
+
 void list_remove(struct list_element* head, int data){
 	struct list_element* head_pointer = head;
 	if(head == NULL){
